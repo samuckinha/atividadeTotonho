221a5a08e5ae77c9c0642b375fa6d228bc6dac47
samuckinha <samuel.schamne.nascimento@escola.pr.gov.br>
2025-09-18 11:31:41 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var nomeCantina = "Cantina da Escola";
 console.log("Bem-vindo à " + nomeCantina)
+
 let salgados = 20;
-console.log("Temos " + salgados + " salgados disponíveis.");
+console.log("Temos " + salgados + " salgados disponíveis.")
+
 const precoSalgado = 5;
-console.log("Cada salgado custa R$" + precoSalgado);
+console.log("Cada salgado custa R$" + precoSalgado)
+
+let.salgados = 20
+console.log("Agora restam " + salgados + " salgados.")
+console.log("A cantina vendeu R$" + totalVendido)
+
+if (true) {
+var testeVar = "Sou var"; 
+ let testeLet = "Sou let"; 
+ console.log(testeVar); // funciona 
+ console.log(testeLet); // funciona
+} 
+console.log(testeVar); // funciona 
+console.log(testeLet); // ERRO
