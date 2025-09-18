ce32c4ce047b6da8a247faf65a3c0f2eafeacac4
samuckinha <samuel.schamne.nascimento@escola.pr.gov.br>
2025-09-18 10:53:13 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+var nomeCantina = "Cantina da Escola";
+console.log("Bem-vindo à " + nomeCantina)
+let salgados = 20;
+console.log("Temos " + salgados + " salgados disponíveis.");
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
